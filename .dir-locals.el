0d8d95ddcc4d49445b0df1786f5ec458585ab415
Sibi Prabakaran <sibi@psibi.in>
2020-11-24 16:59:28 +0530

    Initial commit

---
+++ .dir-locals.el
@@ -0,0 +1,13 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode
+  (org-twbs-link-up . "https://psibi.in/awk/")
+  (org-twbs-link-home . "https://psibi.in/")
+  (org-publish-project-alist . '("awk" 
+                                  :base-directory "~/github/awk/"
+                                  :recursive t
+                                  :base-extension "org"
+                                  :auto-sitemap t
+                                  :publishing-function org-twbs-publish-to-html
+                                  :publishing-directory "~/github/awk/docs/"))))
